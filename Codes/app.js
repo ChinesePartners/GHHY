08c330045193ceb2a71c2eaf2fa7fd22f37c4f46
spy19930412 <812057520@qq.com>
2016-06-24 17:30:48 +0800

    完善了一下前台页面

--- Codes/app.js
+++ Codes/app.js
@@ -4,14 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ueditor = require("ueditor");
+
 
 //tooth 是网站主页面，route 以此为基始
-//Todo 这得怎么给合并一下，看着真闹心啊——spy 
+//Todo 这得怎么给合并一下——spy
 var routes = require('./routes/tooth');
 var users = require('./routes/users');
 var guide = require('./routes/guide');
-var order =require('./routes/order');
-var medical=require('./routes/medical');
+var order = require('./routes/order');
+var medical = require('./routes/medical');
 var doctor = require('./routes/doctors');
 var hospital = require('./routes/hospital');
 var setting = require('./routes/setting');
@@ -36,14 +38,16 @@ app.set('view engine', 'html');
 //app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/admin',admin);
+app.use('/admin', admin);
 
 
 
@@ -77,6 +81,47 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+//后台页面使用ueditor，下面是关于ueditor的相关配置
+//
+//
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json());
+// /ueditor 入口地址配置 https://github.com/netpi/ueditor/blob/master/example/public/ueditor/ueditor.config.js
+// 官方例子是这样的 serverUrl: URL + "php/controller.php"
+// 我们要把它改成 serverUrl: URL + 'ue'
+app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
+
+  // ueditor 客户发起上传图片请求
+
+  if (req.query.action === 'uploadimage') {
+
+    // 这里你可以获得上传图片的信息
+    var foo = req.ueditor;
+    console.log(foo.filename); // exp.png
+    console.log(foo.encoding); // 7bit
+    console.log(foo.mimetype); // image/png
+
+    // 下面填写你要把图片保存到的路径 （ 以 path.join(__dirname, 'public') 作为根路径）
+    var img_url = 'yourpath';
+    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
+  }
+  //  客户端发起图片列表请求
+  else if (req.query.action === 'listimage') {
+    var dir_url = 'your img_dir'; // 要展示给客户端的文件夹路径
+    res.ue_list(dir_url) // 客户端会列出 dir_url 目录下的所有图片
+  }
+  // 客户端发起其它请求
+  else {
+    res.setHeader('Content-Type', 'application/json');
+    // 这里填写 ueditor.config.json 这个文件的路径
+    res.redirect('/ueditor/ueditor.config.json')
+  }
+}));
+
+
 
 app.listen(3008);
 
